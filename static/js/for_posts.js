5e1e08a1aee94ab86a69b9c5a6627760b6c3df9c
bahman_pournazari <bahmanpn@gmail.com>
2023-02-15 19:24:55 +0330

    added card bootstrap for posts html in js

--- static/js/for_posts.js
+++ static/js/for_posts.js
@@ -8,13 +8,37 @@ $.ajax({
             spinner.classList.add('not-visible')
         }, 1500)
         mainRow.innerHTML = "<div id='post-box'></div>"
-        const postBox=document.getElementById('post-box')
+        const postBox = document.getElementById('post-box')
         const data = response.data
         data.forEach((element) => {
-            postBox.innerHTML+=`
-            ${element.title} - <b>${element.body}</b><br>
+            postBox.innerHTML += `
+            <div class="card text-center mb-3">
+              <div class="card-header">
+                ${element.id}
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">${element.body}</p>
+                
+              </div>
+              <div class="card-footer">
+                <div class="row justify-content-center">
+                    <div class="col-1">
+                        <a href="#" class="btn btn-primary">details</a>
+                    </div>
+                    <div class="col-1">
+                        <a href="#" class="btn btn-primary">likes</a>
+                    </div>
+                </div>
+              </div>
+            </div>
             `
         })
+        // data.forEach((element) => {
+        //     postBox.innerHTML+=`
+        //     ${element.title} - <b>${element.body}</b><br>
+        //     `
+        // })
 
     },
     error: function (error) {
