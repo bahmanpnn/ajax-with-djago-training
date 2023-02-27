ad55d201d4c25fbe6a219a87c5e7d0dc685f529c
bahman_pournazari <bahmanpn@gmail.com>
2023-02-27 13:16:20 +0330

    42-edit profile view template and url

--- django_ajax/urls.py
+++ django_ajax/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls'))
+    path('', include('posts.urls')),
+    path('profiles/', include('profiles.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
