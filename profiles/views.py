8f6737c081966527c75a64938dec32f5d8c87b26
bahman_pournazari <bahmanpn@gmail.com>
2023-02-27 12:54:06 +0330

    41-create user profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,26 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Profile
+from .forms import UserProfileForm
+
+
+def profile_page(request):
+    user = Profile.objects.get(user=request.user)
+    form = UserProfileForm(request.POST or None, request.FILES or None, instance=user)
+    if request.POST:
+        if form.is_valid():
+            instance = form.save()
+            # we must not save form directly to use that form and send data with jsonresponse
+            return JsonResponse({
+                'bio': instance.bio,
+                'avatar': instance.avatar.url,
+                'user': instance.user.username
+            })
+    # if request is not Post (is Get) must send form to show it
+    context = {
+        'user': user,
+        'form': form
+    }
+    return render(request, 'profile/main_user_profile.html', context)
